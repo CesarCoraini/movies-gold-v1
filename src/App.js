dec3679026e428a8e3dd38f5200f9fb4ce0014c5
Coraini <coraini1990@gmail.com>
2023-05-09 14:45:53 +0100

    add page not found

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notfound/NotFound';
 
 function App() {
 
@@ -61,6 +62,7 @@ function App() {
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
           <Route path='/Reviews/:movieId' element={<Reviews getMovieData = {getMovieData} movie={movie} reviews = {reviews} setReviews = {setReviews}/> }></Route>
+          <Route path='*' element={<NotFound/>}></Route>
         </Route>
       </Routes>
 
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //baseURL need to be configure before starting
 
 export default axios.create({
-    baseURL: 'https://b4bf-2001-8a0-e4aa-d000-310e-6fa0-4430-f2d7.ngrok-free.app', 
+    baseURL: 'https://95f0-2001-8a0-e4aa-d000-310e-6fa0-4430-f2d7.ngrok-free.app', 
     headers: {"ngrok-skip-browser-warning": "true",
             "Access-Control-Allow-Origin": "*"}
 })
